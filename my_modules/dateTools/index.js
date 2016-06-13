096a07743e8ff5ddb8790f4af66c420c1d06b0cc
Kevin Chisholm <kevinchisholm@Kevins-MacBook-Pro.local>
2016-06-13 18:17:04 -0400

    small code cleanup

--- my_modules/dateTools/index.js
+++ my_modules/dateTools/index.js
@@ -1,27 +1,6 @@
-var days = [
-	'Monday',
-	'Tuesday',
-	'Wednesday',
-	'Thursday',
-	'Friday',
-	'Saturday',
-	'Sunday'
-];
-
-var months = [
-	'January',
-	'February',
-	'March',
-	'April',
-	'May',
-	'June',
-	'July',
-	'August',
-	'September',
-	'October',
-	'November',
-	'December'
-];
+var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+	months = ['January', 'February', 'March', 'April', 'May', 'June', 
+	'July', 'August', 'September', 'October', 'November', 'December'];
 
 var getDays = function () {
 	return days;
